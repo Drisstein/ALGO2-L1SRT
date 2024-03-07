665ac80bef7a14ab1edd87f4573e0b9233295ebd
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-07 14:54:06 +0000

    1/Création+compréhension de l'exo11

--- EXO11.pas
+++ EXO11.pas
@@ -1,1 +1,20 @@
-// Proposer un ALGO permettant de saisir n entiers et d'afficher ld nombre d'entiers égal à 25 parmi eux, n étant saisi.
+// Proposer un ALGO permettant de saisir n entiers et 
+//d'afficher le nombre d'entiers égal à 25 parmi eux, n étant saisi.
+Program EXO10;
+Var 
+    i,n,a,som : integer;
+Begin
+    writeln('Entrer le nombre de fois :');
+    readln(n);
+    
+    For i:=1 To n Do
+        Begin
+            writeln('Entrer un entier:');
+            readln(a);
+            If a>0 Then
+               
+        End;
+    writeln('Le nombre d''entiers égal à 25 est: ',som);
+
+    writeln('A BIENTOT!');
+End.
