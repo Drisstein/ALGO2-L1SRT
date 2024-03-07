3cfd322da1110cdd339623038f5d3896acc9b34f
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-07 00:02:56 +0000

    2/Comprehension et Essayes 1

--- EXO10.pas
+++ EXO10.pas
@@ -1,3 +1,16 @@
 //Proposer un ALGO(pseudo code) puis un programme Pascal permettant de saisir 
 //n entiers et d'afficcher le nombre d'entiers positifs parmi eux,n étant saisi
 
+Var 
+    i,n,a,som : integer;
+Begin
+    writeln('Entrer le nombre de fois :');
+    readln(n);
+    For i:=1 To n Do
+        Begin
+            writeln('Entrer un entier:');
+            readln(a);
+        End;
+        writeln(a);
+     writeln('A BIENTOT!');   
+End.
