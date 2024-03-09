2891dba03ae9d137eca4a2117c4b0cf6364efe60
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-09 23:38:16 +0000

    Résolu:Created variable name `som` to `compt` and added logic to count the number of integers equal to 25.

--- EXO11.pas
+++ EXO11.pas
@@ -1,20 +1,23 @@
-// Proposer un ALGO permettant de saisir n entiers et 
+// EXO11:Proposer un ALGO permettant de saisir n entiers et 
 //d'afficher le nombre d'entiers égal à 25 parmi eux, n étant saisi.
 Program EXO10;
 Var 
-    i,n,a,som : integer;
+    i,n,a,compt : integer;
 Begin
     writeln('Entrer le nombre de fois :');
     readln(n);
-    
+
     For i:=1 To n Do
         Begin
             writeln('Entrer un entier:');
             readln(a);
             If a>0 Then
-               
+                Begin
+                    If a=25 Then
+                    compt := compt+1;
+                End;
         End;
-    writeln('Le nombre d''entiers égal à 25 est: ',som);
+    writeln('Le nombre d''entiers égal à 25 est: ',compt);
 
     writeln('A BIENTOT!');
 End.
