c6321cd5c34d8f11f22696d67e884dd039018866
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-12 12:35:24 +0000

    Màj de l'exo8

--- EXO12.pas
+++
@@ -1,2 +0,0 @@
-//EXO12: Proposer un Algo puis un programme Pascal permettant de saisir n entiers et d'afficher le plus grand et le plus petit parmi eux et la moyenne de tous les entiers, n étant saisis
-//
