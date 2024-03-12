f93038d691ecc9b52da67792ff0cc6abe1d6f108
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-12 00:17:32 +0000

    V2: Moyenne et max réussit

--- EXO12.pas
+++ EXO12.pas
@@ -1,26 +1,31 @@
+
+
 //EXO12: Proposer un Algo puis un programme Pascal permettant de saisir n entiers et d'afficher le plus grand et le plus petit parmi eux et la moyenne de tous les entiers, n étant saisis
 //
 
 Program EXO12;
 Var 
-    i,n,a,som,max,min : integer;
+    i,n,a,max,min,som : integer;
+    moy :  longint;
 Begin
     writeln('Entrer le nombre de fois :');
     readln(n);
-     min:=0;
-     max:=0;
-     som:=0;
+    min := 0;
+    max := 0;
+    som := 0;
     For i:=1 To n Do
         Begin
             writeln('Entrer un entier:');
             readln(a);
             If a>max  Then
-            max:=a;
-            else if a<min Then
-                min:=a;
-            som:=som+a;
+                max := a
+            Else //If a<min Then
+              min := a;
+              som := som+a;
         End;
-    writeln('Le nombre d''entiers égal à 25 est: ',compt);
+    writeln('Le nombre le plus grand nombre est: ',max);
+    writeln('Le nombre le plus petit nombre est: ',min);
+    writeln('La moyenne est: ',som/n:0:2);
 
     writeln('A BIENTOT!');
 End.
