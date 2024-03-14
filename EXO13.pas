5c30ab37a8c0e7514b7dd66fd5a526ad7131471a
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-14 23:11:12 +0000

    1/Creation et ajout du contexte

--- EXO13.pas
+++ EXO13.pas
@@ -1,1 +1,6 @@
-
+// //EXO13: 
+// Notion de controle de saisi(ou validation):
+// Ecrire un Algorithme qui lit un entier positif en utilsant:
+// -TANT QUE....FAIRE
+// -FAIRE....TANQUE
+// -REPETER....JUSQU'A
