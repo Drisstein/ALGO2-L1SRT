34c73dc24dc26ee01647177b3b7ac6f763c8b57a
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-17 23:43:03 +0000

    1/Création et contextualisation

---
+++ EXO15.pas
@@ -0,0 +1,10 @@
+//EXO15:
+//Proposer un algo permettant de calculer et d'afficher la somme 
+//d'entiers saisis. 
+//Pour arreter la saisie des entiers l'utilsateur saisi l'entier zéro (0)
+
+Program EXO15;
+var 
+BEGIN 
+
+END.
