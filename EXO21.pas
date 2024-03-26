fcf01836c30becdeb6c46f3d4cfa43366cefb758
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-26 10:32:55 +0000

    2/Exo20 résolu

--- EXO21.pas
+++ EXO21.pas
@@ -1,1 +1,21 @@
-//proposer un Algo permettant de saisir n (n>) et de faire leur somme . Lorsque a<=0 donc il n'est pas compté.
+
+//proposer un Algo permettant de saisir n (n>) et de faire leur somme . 
+//Lorsque a<=0 donc il n'est pas compté.
+
+Program EXO20;
+Var 
+    i,n,a,som1 : integer;
+Begin
+    writeln('Entrer le nombre de fois :');
+    readln(n);
+    som1 := 0;
+    For i:=1 To n Do
+        Begin
+            writeln('Entrer un entier:');
+            readln(a);
+            If (a>0) Then
+                som1 := som1+a;
+        End;
+    writeln('La somme des nombres positifs saisis est: ',som1);
+    writeln('A BIENTOT!');
+End.
