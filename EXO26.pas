9a76ed50ba0a060956b07764c71e8d116de6e177
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-03-28 00:32:24 +0000

    3/Compilation des programmes et ajout des outputs

--- EXO26.pas
+++ EXO26.pas
@@ -4,22 +4,32 @@ Program EXO26_SORTIES;
 Var 
     i,j : integer;
 Begin
+	
+    writeln('-------------------SORTIE 1----------------------');
+    
     For i:=1 To 5 Do
         Begin
             If i<3 Then
                 writeln('Bonjour');
             write('Bonsoir');
         End;
+    writeln;
+    //BonsoirBonsoirBonsoirBonjour
+    //BonsoirBonjour
+    //Bonsoir
     //////////////////////////////////////////
-    writeln('SORTIE 2');
+    writeln('---------------SORTIE 2-------------------');
     For i:=1 To 5 Do
         If (i<=2) Then
             write('Bonjour')
         Else
             write('Bonsoir');
     writeln('Bonne Nuit');
+    writeln;
+    //SORTIE 2
+    //REPONSE: BonjourBonjourBonsoirBonsoirBonsoirBonne Nuit
     /////////////////////////////////
-    writeln('SORTIE 3');
+    writeln('------------------SORTIE 3------------------------------');
     For i:=1 To 5 Do
         Begin
             j := 1;
@@ -29,13 +39,20 @@ Begin
                     j := j+1
                 End;
         End;
+    writeln;
+    //// 25* writeln('Bonjour');
 
     /////////////////////////////////////////
-    writeln('SORTIE 04');
+    writeln('----------------SORTIE 04------------------');
     For i:=1 To 5 Do
         If i Mod 2=0 Then
             writeln('Bonjour')
-    Else
-        writeln('Bonsoir');
-
+        Else
+            writeln('Bonsoir');
+    //OUTPUT
+    //Bonsoir
+    //Bonjour
+    //Bonsoir
+    //Bonjour
+    //Bonsoir
 End.
