db6833429add3ef28dfaa8992fcf313803215da5
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-04-01 21:54:53 +0000

    3/E10-TP Résolu

--- EXO29-10-TP.pas
+++ EXO29-10-TP.pas
@@ -1,2 +1,14 @@
 //Réaliser un triangle renversé de signes.
-
+Program triangle_de_signe_tp9;
+var i,j,n :integer;
+begin
+	write('Entrer n :');
+	read(n);
+	for i:=1 to n do 
+	begin
+		for j:=1 to n do 
+		write('+');
+		writeln;
+		n:=n-1;
+		end;
+end.
