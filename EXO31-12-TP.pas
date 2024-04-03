3997b3777cf9ab2fc7b26cb26f3443fdd7548a3d
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-04-03 23:29:59 +0000

    1/Création et initialisation du TP12

---
+++ EXO31-12-TP.pas
@@ -0,0 +1,4 @@
+//Ecrire un programme Pascal qui affiche de la maniéresuivante les M notesde N éléves lus au clavier.Considérer une note scolaire est comprise entre  0 et 20 est stocké dans la variable Note.
+
+//--L'Algo qui affiche à l'écran réussi pour les jotes sup=10 , pas réussi sinon
+//--l'Algo aussi va afficher les appreciations suivantes suivant les inervalles de notes sup=10 : (Utilisation SI..SINON..SI..SINON)
