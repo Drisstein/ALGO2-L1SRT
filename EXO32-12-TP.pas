2330bff6560f615672eea2d261b38ddf97aa9e59
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-04-04 03:36:59 +0000

    màj

--- EXO32-12-TP.pas
+++ EXO32-12-TP.pas
@@ -1,19 +1,19 @@
 
 
-//Ecrire un programme Pascal qui affiche de la mani‚re suivante les M notes de N ‚l‚ves lus au clavier.
-//Consid‚rer une note scolaire est comprise entre  0 et 20 est stock‚ dans la variable Note.
-//--L'Algo qui affiche … l'‚cran r‚ussi pour les notes sup=10 , pas r‚ussi sinon
+//Ecrire un programme Pascal qui affiche de la maniï¿½re suivante les M notes de N ï¿½lï¿½ves lus au clavier.
+//Considï¿½rer une note scolaire est comprise entre  0 et 20 est stockï¿½ dans la variable Note.
+//--L'Algo qui affiche ï¿½ l'ï¿½cran rï¿½ussi pour les notes sup=10 , pas rï¿½ussi sinon
 
 //--l'Algo aussi va afficher les appreciations suivantes suivant les inervalles de notes sup=10 : (Utilisation SI..SINON..SI..SINON)
 //////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////--RAISONNEMENT-------------------///////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//sinon si (note<=8) alors Pas  R‚ussi,
-//sinon si  la (note<=10) alors R‚ussi,passable ;
-//sinon si  (note=11) ou (note<=12) alors r‚ussi,Abien;
-//sinon si  (note=14)  ou (note=15) alors r‚ussi,Bien;
-//sinon si (note>=18) alors R‚ussi,tr‚s bien;
+//sinon si (note<=8) alors Pas  Rï¿½ussi,
+//sinon si  la (note<=10) alors Rï¿½ussi,passable ;
+//sinon si  (note=11) ou (note<=12) alors rï¿½ussi,Abien;
+//sinon si  (note=14)  ou (note=15) alors rï¿½ussi,Bien;
+//sinon si (note>=18) alors Rï¿½ussi,trï¿½s bien;
 //////////////////////////////////////////////////////////////////////////////////////////
 Program TP12;
 Var 
@@ -26,34 +26,34 @@ Begin
     ReadLn(m);
 
     For i := 1 To n Do
-        // Lister le nombre d'‚l‚ves
+        // Lister le nombre d'ï¿½lï¿½ves
         Begin
             writeln('Eleve ', i, ' :');
-            // Citer l'‚l‚ve dont nous voulons les notes
+            // Citer l'ï¿½lï¿½ve dont nous voulons les notes
             For j := 1 To m Do
                 // Recueillir les notes 1 par 1
                 Begin
                     Write('Entrer la Note ', j, ' : ');
-                    // La note concern‚e
+                    // La note concernï¿½e
                     readln(note);
                     If (note <= 20) And (note >= 0) Then
                         Begin
                             If (note < 10) Then
-                                writeln('Note: ', note:0:2, ', Pas R‚ussi!')
+                                writeln('Note: ', note:0:2, ', Pas Rï¿½ussi!')
                             Else If (note <= 10) Then
-                                     writeln('Note: ', note:0:2, ', R‚ussi, passable !')
+                                     writeln('Note: ', note:0:2, ', Rï¿½ussi, passable !')
                             Else If (note <= 13) Then
-                                     writeln('Note: ', note:0:2, ', R‚ussi, Assez bien !')
+                                     writeln('Note: ', note:0:2, ', Rï¿½ussi, Assez bien !')
                             Else If (note <= 18) Then
-                                     writeln('Note: ', note:0:2, ', R‚ussi, Tr‚s Bien !')
+                                     writeln('Note: ', note:0:2, ', Rï¿½ussi, Trï¿½s Bien !')
                             Else
-                                writeln('Note: ', note:0:2, ', R‚ussi, TrŠs bien !');
+                                writeln('Note: ', note:0:2, ', Rï¿½ussi, Trï¿½s bien !');
                         End
                     Else if (note>20) or (note<0) then
                         Begin
                             writeln('Saisie incorrecte !');
                             break
-                            // Arrˆte la saisie des notes si une saisie incorrecte est d‚tect‚e
+                            // Arrï¿½te la saisie des notes si une saisie incorrecte est dï¿½tectï¿½e
                         End;
                 End;
         End;
