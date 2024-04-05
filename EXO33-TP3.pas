4011904ea5dfbf60a22618f5282d471c1e80b022
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-04-05 23:58:37 +0000

    2/Initialisation et probléme TP3-Fibonnacci

---
+++ EXO33-TP3.pas
@@ -0,0 +1,4 @@
+//Ecrire un programme Pascal qui calcule et affiche les 10 premiers termes de la suite de Fibonacci.La suite de Fibonacci est définie par:
+//-F0=1
+//-F1=1
+//Fn=Fn-2 + Fn-1 pour n>1 
