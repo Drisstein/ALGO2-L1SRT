e485da379f36e30f473d8ea9f5f83e92d42dda6a
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-04-06 04:44:39 +0000

    This diff adds a program in Pascal that calculates and displays the first n terms of the Fibonacci sequence. The user is prompted to enter the number of terms they want to generate. If the input is less than 1, the program displays an error message and halts. The Fibonacci sequence is generated using a loop, where each term is the sum of the two preceding ones. The current term is updated in each iteration of the loop, and the program writes the term number and its value at each step.

--- EXO33-TP3.pas
+++ EXO33-TP3.pas
@@ -1,5 +1,31 @@
 //Ecrire un programme Pascal qui calcule et affiche les 10 premiers termes de la suite de Fibonacci.La suite de Fibonacci est définie par:
 //-F0=1
 //-F1=1
-//Fn=Fn-2 + Fn-1 pour n>1 
- 
+//Fn=Fn-2 + Fn-1 pour n>1     ex n=10; F10=F8 + F9  
+//F8=1,1,2,3,5,8,13,21
+//F9=34
+//Ainsi on a F10=21+34=55
+
+ Program FibonacciLoop;
+uses crt;
+Var
+    i, n, a, b, c : integer;
+
+Begin
+    Write('Entrer le nombre de termes à générer:');
+    ReadLn(n);
+    If n < 1 Then
+    Begin
+        Writeln('Erreur: le nombre de termes doit être supérieur ou égal à 1.');
+        Halt;
+    End;
+    a := 0;
+    b := 1;
+    For i := 0 to n do
+    Begin
+        c := a + b;
+        a := b;
+        b := c;
+        WriteLn(i, ':', a);
+    End;
+End.
