6f9cff93496e080188a155606766cbbca5af44ae
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-04-13 00:14:47 +0000

    2/Faire un carré avec du writeln (Carré essai 1)

--- EXO36.pas
+++ EXO36.pas
@@ -1,5 +1,18 @@
-program CARRE;
-var i,j :integer;
-begin
-  for 
-end.
+Program CARRE;
+Var 
+    i,j : integer;
+Begin
+   // For i:=1 To 5 Do
+    //    Begin
+    //        For j:=5 downTo 1 Do
+    //            Writeln('*');
+    //            i:=i-1;
+    //    End;
+    
+   writeln( '                 * *  *  *  * *  *   *  *  ');
+   writeln(  '                *                           *');
+   writeln(   '               *                           *');
+   writeln(    '              *                           *');
+   writeln(     '             *                           *');
+    writeln(     '            * *  *  *  *  *  * *  *');
+End.
