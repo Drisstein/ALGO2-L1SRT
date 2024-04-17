6eaa9f918b726e294828c88bfa9a7e5a503d2db0
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-04-17 16:47:40 +0000

    This is a Pascal program that initializes an array of 10 integers and allows the user to input each element. The program then prompts the user to enter a number (n) to search for in the array.

--- TD3-TABLEAUX/TP_04_recherche_1element.pas
+++ TD3-TABLEAUX/TP_04_recherche_1element.pas
@@ -1,1 +1,19 @@
+
 //Proposer un ALgo permettant de saisir un Tableau de 10 entiers puis verifie si un nombre n donné par l'user y figure.
+
+Program TP04;
+Var 
+    Tab10 : array[1..10] Of integer;
+    i,j,n : integer;
+Begin
+    writeln('Saisissez les 10 elements du tableau');
+    For i:=1 To  10 Do
+        Begin
+            write('Entrer Tab[',i,']=');
+            readln(Tab10[i]) ;
+        End;
+    WriteLn('Entrer l''entier recherché :');
+    ReadLn(n);
+    i:=n;
+    WriteLn('Le ',n,'e element  est:',Tab10[i]);
+End.
