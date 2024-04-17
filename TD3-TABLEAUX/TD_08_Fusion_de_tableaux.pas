85625948e9d1db237e31d787e5fd6b7f75abbfc8
Drisstein <145125511+Drisstein@users.noreply.github.com>
2024-04-17 17:03:30 +0000

    TD_08_Fusion

---
+++ TD3-TABLEAUX/TD_08_Fusion_de_tableaux.pas
@@ -0,0 +1,3 @@
+//Ecrire un ALGO qui permet de saisir deux tableaux d'entiers de 5 et 7 entiers.
+//      -Afficher les indices communs aux deux tableaux et contenant le meme entoer
+//      -Creer un tableau d'entiers contenant sucessivement un entier du premier tableau et un entier du 1er tableau et un entier  du second tableau. Le restant d'un des tableaux est inséré à la fin
